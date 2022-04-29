84616b7af84fe0e0128640cc84d99d8c82924c8d
Artem Z <sanchez@MacBook-Pro-Artem-3.local>
2022-04-29 11:29:12 +0300

    remove not used index

--- database/migrations/2022_04_24_071032_create_links_table.php
+++ database/migrations/2022_04_24_071032_create_links_table.php
@@ -22,7 +22,6 @@ return new class extends Migration
             $table->timestamp('expired_at')->nullable();
             $table->timestamps();
 
-            $table->index('original_url');
             $table->index('short_code');
         });
     }
